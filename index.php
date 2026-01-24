237f6efa1cf29b943b176e3837551551a3510e4c
Costas Papaconstantinou <costasp81@gmail.com>
2026-01-24 12:33:17 +0200

    github link

--- index.php
+++ index.php
@@ -770,6 +770,8 @@ if ($view === 'list') {
         <?php endif; ?>
         <footer>
             &copy; <?= date('Y') ?> <?= $displayPoetName ?>. All rights reserved.
+            &bull;
+            <a href="https://github.com/cozats/Poetry-Site-Template" target="_blank" class="admin-link" style="text-decoration: none;">GitHub</a>
             <br>
             <a href="index.php?admin" class="admin-link"><?= $lang['management'] ?></a>
         </footer>
